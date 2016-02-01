ba7f92f90c18044109e95179088cf309cc4a3e32
lukadut <lukadut386@student.polsl.pl>
2016-02-01 23:44:28 +0100

    working with layouts

--- app/src/main/java/amsa/projekt/GPSAdapter.java
+++ app/src/main/java/amsa/projekt/GPSAdapter.java
@@ -6,6 +6,10 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Łukasz on 2016-01-31.
@@ -14,13 +18,11 @@ public class GPSAdapter {
     private Context context;
     private LocationManager locManager;
     private LocationListener locListener;
-    private Location currentLocation;
+    //private Location currentLocation;
+    private final String gpsLocationProvider = LocationManager.GPS_PROVIDER;
 
     public GPSAdapter(Context context) {
         this.context = context;
-    }
-
-    public void getCurrentLocation() {
 
         locManager = (LocationManager) context.getSystemService(Application.LOCATION_SERVICE);
         locListener = new LocationListener() {
@@ -42,11 +44,34 @@ public class GPSAdapter {
 
             @Override
             public void onLocationChanged(Location location) {
-                // TODO Auto-generated method stub
-                currentLocation = location;
+
             }
         };
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locListener);
-        //locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locListener);
+    }
+    public String getLocationInfo(){
+        Location location = getLastKnownLocation();
+        if(location == null){
+            return "GPS niedostępny";
+        }
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+        long timeInMillis = location.getTime();
+        Date date = new Date(timeInMillis);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Ostatnia aktualizacja ").append(sdf.format(date)).append("\r\n")
+                .append(lat>=0? "N":"S").append(Math.abs(lat)).append(", ").append(lon>=0? "E":"W").append(Math.abs(lon));
+        return sb.toString();
+
+    }
+
+    public void requestLocationUpdate() {
+        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5*60*1000, 10, locListener);
+
+    }
+
+    @Nullable
+    public Location getLastKnownLocation(){
+        return locManager.getLastKnownLocation(gpsLocationProvider);
     }
 }
--- app/src/main/java/amsa/projekt/MainActivity.java
+++ app/src/main/java/amsa/projekt/MainActivity.java
@@ -1,21 +1,127 @@
 package amsa.projekt;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    ImageButton gpsRefresh;
+    GPSAdapter gpsAdapter;
+    Button addNewStation, closestStation, allStations, stationsMap;
+
+
+    final String networkLocationProvider = LocationManager.NETWORK_PROVIDER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textView);
+
+        gpsAdapter = new GPSAdapter(this);
+
+        gpsRefresh = (ImageButton)findViewById(R.id.imageButton);
+        gpsRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    gpsAdapter.requestLocationUpdate();
+                setText(gpsAdapter.getLocationInfo());
+            }
+        });
+
+        addNewStation = (Button)findViewById(R.id.buttonNewStation);
+        addNewStation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = intentFactory(NewStation.class);
+                startActivity(i);
+            }
+        });
+
+        allStations = (Button)findViewById(R.id.buttonAllStations);
+        allStations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        closestStation = (Button)findViewById(R.id.buttonClosest);
+        closestStation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLastKnownLocation();
+            }
+        });
+
+        stationsMap = (Button)findViewById(R.id.buttonMap);
+        stationsMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void setText(String t){
+        tv.setText(t);
+    }
+
+    public Location getLastKnownLocation() {
+        Location lastKnownLocation_byGps = gpsAdapter.getLastKnownLocation();
+
+        if (lastKnownLocation_byGps == null) {
+            setText("GPS nie dostępny");
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("GPS wyłączony, nie można ustalić lokalizacji");
+            dlgAlert.setTitle("Usługa GPS");
+            dlgAlert.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //dismiss the dialog
+                        }
+                    });
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+            return null;
+        }
+        return lastKnownLocation_byGps;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        setText(gpsAdapter.getLocationInfo());
 //        Intent i = new Intent(MainActivity.this, MapsActivity.class);
 //        startActivity(i);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private Intent intentFactory(Class clz){
+        return new Intent(MainActivity.this,clz);
     }
 }
